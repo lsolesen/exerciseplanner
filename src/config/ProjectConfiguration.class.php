905c660fd59d7d74f430c8b8355820105c0894fc
Nathanael D Noblet <nnoblet@nobletdesign.com>
2009-01-22 18:09:57 +0000

    reset the externals

--- src/config/ProjectConfiguration.class.php
+++ src/config/ProjectConfiguration.class.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once 'lib/symfony/lib/autoload/sfCoreAutoload.class.php';
+# FROZEN_SF_LIB_DIR: /var/www/production/sfweb/www/cache/symfony-for-release/1.2.2/lib
+
+require_once dirname(__FILE__).'/../lib/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
