c5339a593afc14ced3466b5fbf6dc1b60ae9bd60
Lars Olesen <lars@intraface.dk>
2009-02-28 22:42:56 +0000

    Removed unused tests. And fixed the muscles test. Muscles are only available to the logged in users for the time being.

--- src/test/functional/frontend/musclesActionsTest.php
+++ src/test/functional/frontend/musclesActionsTest.php
@@ -6,14 +6,14 @@ $browser = new sfTestFunctional(new sfBrowser());
 
 $browser->
   get('/muscles/index')->
-
+  /*
   with('request')->begin()->
     isParameter('module', 'muscles')->
     isParameter('action', 'index')->
   end()->
-
+  */
   with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
+    isStatusCode(401)->
+    /*checkElement('body', '!/This is a temporary page/')->*/
   end()
 ;
